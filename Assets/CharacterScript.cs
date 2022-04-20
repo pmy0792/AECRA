262cafd5af570fd5145d5d3cdd1bad86bfa91256
pmy0792 <pmy0792@naver.com>
2022-04-20 15:33:09 +0300

    Modified player movement

--- Assets/CharacterScript.cs
+++ Assets/CharacterScript.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class CharacterScript : MonoBehaviour
 {
     public float speed = 5;
+    Rigidbody2D body;
     float horizontal;
     public SpriteRenderer rend;
-    
+    private float power=1f;
     public Animator animator;
     void Start()
     { 
+        body=GetComponent<Rigidbody2D>();
         rend=GetComponent<SpriteRenderer>();
         animator=GetComponent<Animator>();
         animator.SetBool("Attack",false);
@@ -20,13 +22,16 @@ public class CharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.UpArrow)){
+            body.velocity=Vector2.up*power;
+        }
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         Vector2 pos = transform.position;
 
         pos.x += speed*h*Time.deltaTime;
-        pos.y += speed*v*Time.deltaTime;
+        //pos.y += speed*v*Time.deltaTime;
 
         transform.position = pos;
 
