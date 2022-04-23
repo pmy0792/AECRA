99d861630971c4bdd179d01887aa839330811700
pmy0792 <pmy0792@naver.com>
2022-04-23 17:09:44 +0300

    item error fixed

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     public int ScoreUp=50;
+    public GameObject gameobject;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //gameobject=GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -20,6 +21,7 @@ public class ItemScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag=="Player"){
             ScoreScript.score+=ScoreUp;
+            //gameObject.SetActive(false);
             gameObject.SetActive(false);
         }    
     }
