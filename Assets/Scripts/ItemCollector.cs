5211a2bdd16398d2af4919141c38aa001bb21340
randrominie <rassa.kundrotaite@gmail.com>
2022-04-28 10:03:30 +0300

    vaccine text effect

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -6,6 +6,12 @@ public class ItemCollector : MonoBehaviour
 {
     public static int coins = 0;
 
+    public bool collecting_vaccine;
+    public bool collecting_herb;
+    public bool collecting_coin;
+    float timer;
+    GameObject vaccine_effect;
+
     [SerializeField] private TMP_Text coinsText;
 
     [SerializeField] private AudioSource coinCollectionSoundEffect;
@@ -13,7 +19,12 @@ public class ItemCollector : MonoBehaviour
 
     private void Start() {
         coins = 0;
+        vaccine_effect=transform.Find("TextEffect").gameObject;
+        collecting_coin=false;
+        collecting_vaccine=false;
+        collecting_herb=false;
         PlayerPrefs.SetInt("collect", 0); 
+        timer=0;
     }
     private void Update() {
         
@@ -24,17 +35,23 @@ public class ItemCollector : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
+            collecting_coin=true;
             coinCollectionSoundEffect.Play();
             Destroy(collision.gameObject);
             coins=coins+5;
             
             PlayerPrefs.SetInt("collect", 1);
+            collecting_coin=false;
         }
 
         else if (collision.gameObject.CompareTag("Vaccine"))
         {
+            collecting_vaccine=true;
+            vaccine_effect.SetActive(true);
+            
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
+            
             coins=coins+15;
             
             PlayerPrefs.SetInt("collect", 1);
@@ -42,8 +59,11 @@ public class ItemCollector : MonoBehaviour
 
         else if (collision.gameObject.CompareTag("Herb"))
         {
+            collecting_herb=true;
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
+            collecting_herb=false;
+
             coins=coins+10;
             
             PlayerPrefs.SetInt("collect", 1);
