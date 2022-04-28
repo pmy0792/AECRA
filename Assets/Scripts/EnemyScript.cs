cc4cf31c2d137545ca7abf4beb5d3b816f158123
pmy0792 <pmy0792@naver.com>
2022-04-28 03:05:13 +0300

    Integrated enemyscript.cs with previous one

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public int hp = 100;
+    float timer=0f;
+    public float speed=3.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,18 @@ public class EnemyScript : MonoBehaviour
         {
             Debug.Log("Destroy");
             Destroy(gameObject);
+        }
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right*speed*Time.deltaTime);
+        
+        if (speed!=3f){
+            timer+=Time.deltaTime;
+            if (timer>1){
+                speed=3f;
+                timer=0;
+            }
         }
     }
 }
