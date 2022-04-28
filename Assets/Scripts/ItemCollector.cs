5fbe9d24040b06f9271eb3fd4721c34e858d75f3
pmy0792 <pmy0792@naver.com>
2022-04-28 03:04:36 +0300

    Add text effect when collected vaccine item

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -5,6 +5,11 @@ using TMPro;
 public class ItemCollector : MonoBehaviour
 {
     public static int coins = 0;
+    public bool collecting_vaccine;
+    public bool collecting_herb;
+    public bool collecting_coin;
+    float timer;
+    GameObject vaccine_effect;
 
     [SerializeField] private TMP_Text coinsText;
 
@@ -13,36 +18,48 @@ public class ItemCollector : MonoBehaviour
 
     private void Start() {
         coins = 0;
+        vaccine_effect=transform.Find("TextEffect").gameObject;
+        collecting_coin=false;
+        collecting_vaccine=false;
+        collecting_herb=false;
         PlayerPrefs.SetInt("collect", 0); 
+        timer=0;
     }
     private void Update() {
         
         coinsText.text = "Coins: " + coins;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
             GameObject objectToDestroy = collision.gameObject;
-
+            collecting_coin=true;
             coinCollectionSoundEffect.Play();
             Destroy(collision.gameObject);
             coins++;
             
             PlayerPrefs.SetInt("collect", 1);
+            collecting_coin=false;
         }
 
         else if (collision.gameObject.CompareTag("Vaccine"))
         {
+            collecting_vaccine=true;
+            vaccine_effect.SetActive(true);
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
+            
         }
 
         else if (collision.gameObject.CompareTag("Herb"))
         {
+            collecting_herb=true;
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
+            collecting_herb=false;
+        }
+
         }
     }
-}
